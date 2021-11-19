61cdbb179383aaba918dd99c4c1b0632d453a203
cs <cs@nuapi.com>
2021-11-19 11:13:24 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+// 配置信息
+module.exports = {
+  fromDisplayText: "God's message", // 收件箱展示的来件人名字
+  fromDisplaySubText: '生活很苦但你很甜', // 收件箱展示的次级标题
+  user: '1756297001@qq.com', // 发送者邮箱
+  pass: 'nqnaienyywohebfd', // 发送者邮箱MTP协议密码
+  to: '1724033718@qq.com', // 发送到谁，填邮箱
+  weatherKey: '187d3e74b2014b5eb9752259f86b58e7', // 和风天气key
+  location: '101050510',  // 和风天气-绥棱的id
+  type: '1,3,9', // 和风天气-生活指数type
+  tianXingKey: '1cf628da9bd9175b47cd8267f00741af', // 天行数据的key
+  startDay: '1999-06-15', // 出生的日期
+}
